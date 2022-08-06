20d7ca2e28935f0d1af27f76d890842807c61f90
Mark Connelly <com0004@gmail.com>
2022-08-06 15:01:41 +1000

    Update demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,30 @@
+<script lang="ts">
+	import { browser } from '$app/env';
+	if (browser) import('$lib');
+</script>
+
 <h1>Ripple Class Demo</h1>
 
+<h2>Setup</h2>
+<p>Import the ripple library.</p>
+<pre>{`import "ripple-class";`}</pre>
+
 <h2>Default</h2>
+<p>The default ripple is white.</p>
 <div class="example">
 	<pre>{`<button class="ripple">Click Me!</button>`}</pre>
-	<div class="center"><button>Click Me!</button></div>
+	<div class="center"><button class="ripple">Click Me!</button></div>
 </div>
 
 <h2>Colored</h2>
 <div class="example">
 	<pre>{`<button class="ripple">Click Me!</button>`}</pre>
-	<div class="center"><button>Click Me!</button></div>
+	<div class="center"><button class="ripple">Click Me!</button></div>
 </div>
 
 <style>
 	/* Heading */
-	h1,
-	h2 {
+	* {
 		font-family: Arial, Helvetica, sans-serif;
 	}
 
@@ -39,47 +48,32 @@
 
 	/* Buttons */
 	button {
-		align-items: center;
-		background-color: #ffffff;
-		border: 1px solid rgba(0, 0, 0, 0.1);
-		border-radius: 0.25rem;
-		box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
+		appearance: button;
+		backface-visibility: hidden;
+		background-color: #405cf5;
+		border-radius: 6px;
+		border-width: 0;
+		box-shadow: rgba(50, 50, 93, 0.1) 0 0 0 1px inset, rgba(50, 50, 93, 0.1) 0 2px 5px 0,
+			rgba(0, 0, 0, 0.07) 0 1px 1px 0;
 		box-sizing: border-box;
-		color: rgba(0, 0, 0, 0.85);
+		color: #fff;
 		cursor: pointer;
-		display: inline-flex;
-		font-family: system-ui, -apple-system, system-ui, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-		font-size: 16px;
-		font-weight: 600;
-		justify-content: center;
-		line-height: 1.25;
-		margin: 0;
-		min-height: 3rem;
-		padding: calc(0.875rem - 1px) calc(1.5rem - 1px);
+		font-family: -apple-system, system-ui, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, sans-serif;
+		font-size: 100%;
+		height: 44px;
+		line-height: 1.15;
+		margin: 12px 0 0;
+		outline: none;
+		overflow: hidden;
+		padding: 0 25px;
 		position: relative;
-		text-decoration: none;
-		transition: all 250ms;
+		text-align: center;
+		text-transform: none;
+		transform: translateZ(0);
+		transition: all 0.2s, box-shadow 0.08s ease-in;
 		user-select: none;
 		-webkit-user-select: none;
 		touch-action: manipulation;
-		vertical-align: baseline;
-		width: auto;
-	}
-	button:hover,
-	button:focus {
-		border-color: rgba(0, 0, 0, 0.15);
-		box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
-		color: rgba(0, 0, 0, 0.65);
-	}
-	button:hover {
-		transform: translateY(-1px);
-	}
-	button:active {
-		background-color: #f0f0f1;
-		border-color: rgba(0, 0, 0, 0.15);
-		box-shadow: rgba(0, 0, 0, 0.06) 0 2px 4px;
-		color: rgba(0, 0, 0, 0.65);
-		transform: translateY(0);
 	}
 
 	/* Example Area */
