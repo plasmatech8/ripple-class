73cbdd335d318587e42f9193cd8620cefcd5225e
Mark Connelly <com0004@gmail.com>
2022-12-26 22:56:14 +1100

    Add SEO

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Ripple Class</title>
+	<meta
+		name="description"
+		content="Demo for the ripple-class NPM package with instructions for setup, usage and customization."
+	/>
+</svelte:head>
+
+<slot />
